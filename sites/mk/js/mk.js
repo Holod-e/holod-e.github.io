e25a137ed60b42e85a5bf616743f44e702e69cb3
Evhenii Holodniak <holodniak.e@gmail.com>
2016-07-12 23:47:45 +0300

    mask

--- sites/mk/js/mk.js
+++ sites/mk/js/mk.js
@@ -134,7 +134,7 @@ var p1 = {
     },
     p29 = {
     name: "Jason",
-    img: 'img/Jason.png' 
+    img: 'img/maska.png' 
     };
 // Использование
 var fighters = [p1, p2, p3, p4, p5, p6, p7 ,p8, p9, p10, p11, p12 ,p13 ,p14, p15, p16, p17, p18, p19,p20 ,p21 ,p22 ,p23 , p24, p25, p26, p27, p28, p29];
