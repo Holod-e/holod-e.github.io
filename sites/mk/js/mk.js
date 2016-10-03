7a70e68ca1dbf1867a9601fb0419c954c6391469
Евгений Голодняк < >
2016-10-03 13:17:34 +0300

    add random2

--- sites/mk/js/mk.js
+++ sites/mk/js/mk.js
@@ -176,7 +176,7 @@ var generate2 = function(){
         if (randomNumber > 0.6666) {
             styleNumber = 3;
         }
-        var el = "<li><button>win</button><span class='styleNumber'>" + styelNumber +"</span><img src='" + player2[j].img + "'>" + player2[j].name + "</li>";
+        var el = "<li><button>win</button><span class='styleNumber'>" + styleNumber +"</span><img src='" + player2[j].img + "'>" + player2[j].name + "</li>";
         $('#fighters2').append(el);
     };
 };
